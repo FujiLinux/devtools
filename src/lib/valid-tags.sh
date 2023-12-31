0c91303d48e50df50fd765492e7f66b1a604b66c
Evan Welsh <contact@evanwelsh.com>
2023-12-30 23:00:50 -0600

    Add support for aarch64

--- src/lib/valid-tags.sh
+++ src/lib/valid-tags.sh
@@ -6,20 +6,21 @@
 # shellcheck disable=2034
 DEVTOOLS_VALID_ARCHES=(
 	x86_64
+	aarch64
 	any
 )
 
 # shellcheck disable=2034
 DEVTOOLS_VALID_TAGS=(
-	core-x86_64 core-any
-	core-staging-x86_64 core-staging-any
-	core-testing-x86_64 core-testing-any
-	extra-x86_64 extra-any
-	extra-staging-x86_64 extra-staging-any
-	extra-testing-x86_64 extra-testing-any
+	core-x86_64 core-aarch64 core-any
+	core-staging-x86_64 core-staging-aarch64 core-staging-any
+	core-testing-x86_64 core-testing-aarch64 core-testing-any
+	extra-x86_64 extra-aarch64 extra-any
+	extra-staging-x86_64 extra-staging-aarch64 extra-staging-any
+	extra-testing-x86_64 extra-testing-aarch64 extra-testing-any
 	multilib-x86_64
 	multilib-testing-x86_64
 	multilib-staging-x86_64
-	kde-unstable-x86_64 kde-unstable-any
-	gnome-unstable-x86_64 gnome-unstable-any
+	kde-unstable-x86_64 kde-unstable-aarch64 kde-unstable-any
+	gnome-unstable-x86_64 gnome-unstable-aarch64 gnome-unstable-any
 )
